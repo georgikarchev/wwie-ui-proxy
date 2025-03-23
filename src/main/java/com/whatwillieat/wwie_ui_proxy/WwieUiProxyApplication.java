1ce3c8aedf9945aa5b13a24b9644851dddd70401
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-23 17:05:24 +0200

    Initiate Spring Boot App

---
+++ src/main/java/com/whatwillieat/wwie_ui_proxy/WwieUiProxyApplication.java
@@ -0,0 +1,13 @@
+package com.whatwillieat.wwie_ui_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwieUiProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwieUiProxyApplication.class, args);
+	}
+
+}
